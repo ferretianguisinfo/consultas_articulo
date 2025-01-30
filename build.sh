38f1fa3e6593deb44ce2b75fc2a84e655433a4df
ferretianguisinfo <ferretianguisinfo@gmail.com>
2025-01-30 10:41:06 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,35 @@
-#!/bin/bash
-# Instalar ODBC Driver 17
-apt-get update && apt-get install -y \
+FROM ubuntu:20.04
+
+# Instalar dependencias necesarias
+RUN apt-get update && apt-get install -y \
+    curl \
+    gnupg \
     unixodbc \
     unixodbc-dev \
     odbcinst \
     odbcinst1debian2 \
-    libodbc1 \
-    curl \
-    gnupg
+    libodbc1
 
-# Agregar claves y repositorio de Microsoft ODBC
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-add-apt-repository "$(curl -s https://packages.microsoft.com/config/ubuntu/20.04/prod.list)"
+# Agregar claves y repositorio de Microsoft para ODBC
+RUN curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
+    && add-apt-repository "$(curl -s https://packages.microsoft.com/config/ubuntu/20.04/prod.list)"
 
-# Instalar Microsoft ODBC Driver 17
-apt-get update && apt-get install -y msodbcsql17
+# Instalar ODBC Driver 17
+RUN apt-get update && apt-get install -y msodbcsql17
 
-# Ejecutar la aplicación
-python app.py
+# Instalar Python y dependencias
+RUN apt-get install -y python3 python3-pip
+
+# Copiar archivos del proyecto
+WORKDIR /app
+COPY . /app
+
+# Instalar librerías de Python
+RUN pip3 install -r requirements.txt
+
+# Exponer el puerto
+EXPOSE 5000
+
+# Comando para ejecutar la aplicación
+CMD ["python3", "app.py"]
+
