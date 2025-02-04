7e5a43b65838819cff31ce939478d079498ca3c8
ferretianguisinfo <ferretianguisinfo@gmail.com>
2025-02-04 09:02:04 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,35 +1,18 @@
-FROM ubuntu:20.04
+#!/usr/bin/env bash
+# Actualiza los paquetes
+apt-get update
 
-# Instalar dependencias necesarias
-RUN apt-get update && apt-get install -y \
-    curl \
-    gnupg \
-    unixodbc \
-    unixodbc-dev \
-    odbcinst \
-    odbcinst1debian2 \
-    libodbc1
+# Instala los paquetes necesarios para ODBC Driver 17
+apt-get install -y curl apt-transport-https gnupg
 
-# Agregar claves y repositorio de Microsoft para ODBC
-RUN curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
-    && add-apt-repository "$(curl -s https://packages.microsoft.com/config/ubuntu/20.04/prod.list)"
+# Agrega el repositorio de Microsoft
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
-# Instalar ODBC Driver 17
-RUN apt-get update && apt-get install -y msodbcsql17
+# Instala el driver
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
 
-# Instalar Python y dependencias
-RUN apt-get install -y python3 python3-pip
-
-# Copiar archivos del proyecto
-WORKDIR /app
-COPY . /app
-
-# Instalar librerías de Python
-RUN pip3 install -r requirements.txt
-
-# Exponer el puerto
-EXPOSE 5000
-
-# Comando para ejecutar la aplicación
-CMD ["python3", "app.py"]
+# Limpia el caché
+apt-get clean
 
